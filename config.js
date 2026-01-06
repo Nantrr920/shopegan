ae5713a864e1ca525d31fc57b1712cc25d6576a3
Nantrr920 <waaslempire@gmail.com>
2026-01-06 22:36:37 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const siteConfig = {
+  "siteName": "Gene_Z",
+  "logoUrl": "logo.png",
+  "telegramUsername": "Generation_Z_off",
+  "miniAppUrl": "",
+  "instagramUrl": "https://www.instagram.com/generation_z_off?igsh=MTcydnNiYm10NDBmcQ%3D%3D&utm_source=qr",
+  "canalUrl": "https://t.me/+G6BC710lCnsyZmE0",
+  "contactUsername": "Generation_Z_off",
+  "infoTitle": "À propos de Coffee Egan",
+  "infoMainText": "Bienvenue sur notre shop ! Retrouvez nos meilleurs produits sélectionnés avec soin.",
+  "infoDeliveryTitle": "📦 Livraison",
+  "infoDeliveryText": "Livraison rapide et discrète partout en France. Délai de 24-48h.",
+  "infoPaymentTitle": "💳 Paiement",
+  "infoPaymentText": "Modes de paiement sécurisés disponibles : Crypto, espèces.",
+  "categories": [
+    "hash",
+    "weed",
+    "coke"
+  ]
+};
