6c42bdc5d146d9af5ac84eaa485ce890cda5d51d
Nantrr920 <waaslempire@gmail.com>
2026-01-06 23:16:43 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,38 @@
+const TELEGRAM_USERNAME="Generation_Z_off";
+
+const products = [
+  {
+    "id": 1,
+    "name": "Caliplates Fozen",
+    "category": "fleurs",
+    "image": "https://officiallamoussehash.com/wp-content/uploads/2025/11/Buy-Cali-Plates-Frozen-Hash-At-Cali-Plates-Hash-Online-Store-2-removebg-preview-1-247x296.png",
+    "video": "",
+    "description": "Produit premium de qualité supérieure. Sélection Cali exclusive.",
+    "varieties": [
+      "GELATO",
+      "WEDDING PIE"
+    ],
+    "prices": [
+      {
+        "amount": "150 €",
+        "quantity": "5g"
+      },
+      {
+        "amount": "280 €",
+        "quantity": "10g"
+      },
+      {
+        "amount": "800 €",
+        "quantity": "25g"
+      },
+      {
+        "amount": "1200 €",
+        "quantity": "50g"
+      },
+      {
+        "amount": "2000 €",
+        "quantity": "100g"
+      }
+    ]
+  }
+];
